cd9d2fd2e0a83b143bd6a94d63d8f7271bca120e
youngsupark1 <eddy364885@gmail.com>
2024-06-18 11:34:47 +0900

    ✨ update : App.jsx에 Map 컴포넌트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { kcisaApi } from './api/kcisa.api';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './query-client/queryClient';
-
 import Map from './components/Map';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>App</div>
+      <Map />
     </QueryClientProvider>
   );
 };
