fc394324d33e91f43b7d538b515234f1500d2e68
youngsupark1 <eddy364885@gmail.com>
2024-06-18 10:55:51 +0900

    ✨ update : map.jsx에 지도 api 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import MainMap from './components/MainMap';
+
 import Map from './components/Map';
 
 const App = () => {
   return (
     <div>
-      <h2 className="my-4 text-2xl font-bold text-center">아트나비</h2>
-      <div className="flex justify-center">
-        <Map />
-          <MainMap />
-      </div>
+      <Map />
     </div>
   );
 };
