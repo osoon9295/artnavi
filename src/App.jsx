8ef4e6a09b9628ad493eabd7f6302583dc5d895e
Boyoung Lee <genuss040822@gamil.com>
2024-06-18 11:13:05 +0900

    ✨ update(kcisa.api.js) : getShow 함수 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { kcisaApi } from './api/kcisa.api';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from './query-client/queryClient';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>App</div>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
