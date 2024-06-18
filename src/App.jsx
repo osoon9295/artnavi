a59b5ee811efcb56c7bc5c461d177742a6a4ed30
hwijinkim22 <bout9396@naver.com>
2024-06-18 19:04:38 +0900

    updata: 키워드로 장소 검색하기 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { kcisaApi } from './api/kcisa.api';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './query-client/queryClient';
 import BasicMap from './components/BasicMap';
--- src/components/BasicMap.jsx
+++ src/components/BasicMap.jsx
@@ -1,7 +1,43 @@
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import logoImage from '../logo/artnavi.png';
+import useKakaoLoader from './UseKakaoLoader';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function BasicMap() {
+  useKakaoLoader();
+
+  const [info, setInfo] = useState(null); 
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState(null);
+  const [keyword, setKeyword] = useState("서울 미술관");
+
+  useEffect(() => {
+    if(!map) return; 
+
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch("서울 미술관", (data, status, _pagination) => {
+      if(status === kakao.maps.services.Status.OK) {
+        const bounds = new kakao.maps.LatLngBounds();
+        let markers = [];
+
+        for (let i = 0; i < data.length; i++) {
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x,
+            },
+            content: data[i].place_name,
+          })
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
+        }
+        setMarkers(markers);
+        map.setBounds(bounds);
+      }
+    })
+  }, [map, keyword]);
+
   return (
     <>
       <div className="w-[1440px] h-[920px] flex m-auto">
@@ -9,15 +45,28 @@ export default function BasicMap() {
           <img src={logoImage}></img>
         </div>
         <div>
-          <Map // 지도를 표시할 Container
+          <Map
             id="map"
             className="w-[1120px] h-[920px]"
+            level={3}
+            onCreate={setMap}
             center={{
-              // 지도의 중심좌표
               lat: 37.564214,
               lng: 127.001699
             }}
-          />
+          >
+             {markers.map((marker) => (
+        <MapMarker
+          key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+          position={marker.position}
+          onClick={() => setInfo(marker)}
+        >
+          {info &&info.content === marker.content && (
+            <div style={{color:"#000"}}>{marker.content}</div>
+          )}
+        </MapMarker>
+      ))}
+    </Map>
         </div>
       </div>
     </>
