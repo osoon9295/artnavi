ae9847c995c4d6e03037f45965ed42e940b8a177
Joseph Park <rkfnahs12e@gmail.com>
2024-06-18 14:23:51 +0900

    refactor : useStore -> useShowStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
-import React from 'react';
-import { kcisaApi } from './api/kcisa.api';
 import { QueryClientProvider } from '@tanstack/react-query';
-import queryClient from './query-client/queryClient';
+import React from 'react';
 import Map from './components/Map';
+import queryClient from './query-client/queryClient';
+import { useStore } from './zustand/store';
 
 const App = () => {
+  const { showInfo } = useStore();
+
   return (
     <QueryClientProvider client={queryClient}>
       <Map />
