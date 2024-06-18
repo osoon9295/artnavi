7c4b249b46f13ccfede2b830a9a52740e6e8b0dd
hwijinkim22 <bout9396@naver.com>
2024-06-18 10:37:10 +0900

    fix: 컴포넌트명 변경 Map.jsx => MainMap.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Map from './components/Map';
+import MainMap from './components/MainMap';
 
 const App = () => {
   return (
     <div>
       <h2 className="my-4 text-2xl font-bold text-center">아트나비</h2>
       <div className="flex justify-center">
-        <Map />
+        <MainMap />
       </div>
     </div>
   );
