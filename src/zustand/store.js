3192f92ce15c35d6afc960f9f9e5206dd0a984af
Joseph Park <rkfnahs12e@gmail.com>
2024-06-18 11:15:42 +0900

    chore : 주석 추가

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -2,12 +2,27 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 const useStore = create(
-  persist((set) => ({
-    shows: [],
-    setShows: (shows) => set((state) => ({ shows: [...state, shows] })),
-    showInfo: {},
-    setShowInfo: (info) => set(() => ({ showInfo: info }))
-  }))
+  persist(
+    (set) => ({
+      shows: [],
+
+      /**
+       * @param {shows[]} shows 전시 목록 입력
+       */
+      setShows: (shows) => set((state) => ({ shows: [...state.shows, shows] })),
+
+      showInfo: {},
+
+      /**
+       * @param {{...}} info 전시에 대한 정보
+       */
+      setShowInfo: (info) => set(() => ({ showInfo: info }))
+    }),
+    {
+      name: 'my-storage',
+      getStorage: () => localStorage
+    }
+  )
 );
 
 export default useStore;
