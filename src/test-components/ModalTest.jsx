ae577ec67536239a00673927876e52b2c5d80319
computer-note <planomenes@gmail.com>
2024-06-18 14:53:16 +0900

    update (ModalTest.jsx) : 테스트 코드 뼈대 작성

--- src/test-components/ModalTest.jsx
+++ src/test-components/ModalTest.jsx
@@ -1,5 +1,21 @@
+import { useModal } from './../contexts/modal.context';
+
 function ModalTest() {
-  return <>ModalTest</>;
+  const modal = useModal();
+
+  function handllOpenButtonClick() {
+    modal.open();
+  }
+
+  function handleCloseButtonClick() {
+    modal.close();
+  }
+  return (
+    <main>
+      <button onClick={handllOpenButtonClick}>모달오픈</button>
+      <button onClick={handleCloseButtonClick}>모달닫기</button>
+    </main>
+  );
 }
 
 export default ModalTest;
