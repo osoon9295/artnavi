d5a7566c7680f7dffa2066fc8072a4e1e4e71719
김도희(React_5기) <doeheekim_react@React5-MacBook-Air.local>
2024-06-18 17:01:37 +0900

    :sparkles: update: 날씨, 온도 API 연결

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoImage from '../logo/artnavi.png';
 import { useEffect } from 'react';
 import CardList from './ExhibitList';
+import axios from 'axios';
+
+const weatherAPIKey = "a42d7ee85839a67d4fe350775f82d621";
 
 export default function Map() {
+const [weather, setWeather] = useState({})
+  
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
@@ -14,6 +19,42 @@ export default function Map() {
     const map = new kakao.maps.Map(container, options);
   }, []);
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeather(lat, lon);
+    });
+  }, []);
+  
+  const getWeather = async(lat, lon) => {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
+      );
+
+      // id 찾아서 매칭 후 description 한글 번역된 거 가져오기
+      const weatherId = res.data.weather[0].id;
+      // const weatherKo = weatherDescKo[weatherId];
+      // 날씨 아이콘 가져오기
+      const weatherIcon = res.data.weather[0].icon;
+      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      // 소수점 버리기
+      const temp = Math.round(res.data.main.temp);
+
+      setWeather({
+        description: weatherKo,
+        name: cityName,
+        temp: temp,
+        icon: weatherIconAdrs,
+      });
+    } catch (err){ 
+      console.error(err);
+    }
+  };
+  
+  console.log(weather);
+  
   return (
     <>
       <div className="w-[1440px] h-[920px] flex m-auto">
